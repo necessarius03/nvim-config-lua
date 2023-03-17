3420dd61ac751305701238bb89f4310c4f0100d9
Andrew <contach.ngtrh@gmail.com>
2023-03-17 14:09:42 +0700

    change packer to lazy

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,25 +1,24 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-tree/nvim-tree.lua'
-  use 'nvim-tree/nvim-web-devicons'
-  use 'nvim-lualine/lualine.nvim'
+local plugins = { 
+   'nvim-tree/nvim-tree.lua',
+   'nvim-tree/nvim-web-devicons',
+   'nvim-lualine/lualine.nvim',
   -- use 'folke/tokyonight.nvim'
-  use 'navarasu/onedark.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
-  use {
+   'navarasu/onedark.nvim',
+   'nvim-treesitter/nvim-treesitter',
+   {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
@@ -27,25 +26,23 @@ return require('packer').startup(function(use)
     "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
     "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
     "L3MON4D3/LuaSnip", -- Snippets plugin
-    "onsails/lspkind.nvim"
-  }
-  use {
+    "onsails/lspkind.nvim",
+   },
+   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use 'glepnir/dashboard-nvim'
-  use 'akinsho/toggleterm.nvim'
-  use {
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+   },
+   'glepnir/dashboard-nvim',
+   'akinsho/toggleterm.nvim',
+   {
     'akinsho/bufferline.nvim',
     tag = "v3.*",
-    requires = 'nvim-tree/nvim-web-devicons'
-  }
-  use 'folke/which-key.nvim'
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+    dependencies = 'nvim-tree/nvim-web-devicons'
+   },
+   'folke/which-key.nvim'
+ }
 
+local opts = {}
+
+require("lazy").setup(plugins, opts)
